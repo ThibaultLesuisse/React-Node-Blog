78321adc1b8c57e5b4e35a2c743b35e97976258a
Thibault <thibault@localhost.localdomain>
2018-02-20 20:27:47 +0100

    chore()

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -2,7 +2,7 @@ const isAuthenticated = require('../helpers/isAuthenticated');
 const Post = require('../models/Post')
 
 exports.blog = (req, res) => {
-    isAuthenticated?res.render('blog', {
+    isAuthenticated(req, res)?res.render('blog', {
         post: Post.find({})
     }):res.render('home');
 }
--- helpers/isAuthenticated.js
+++ helpers/isAuthenticated.js
@@ -1,7 +1,10 @@
 let sessions = require('../session');
 
 module.exports = (req, res) => {
-    if(sessions.indexOf(req.cookies.session) != -1){
+    let _session = [];
+    _session = sessions;
+    console.log(sessions);
+    if(_session.indexOf(req.cookies.session) != -1){
         return true;
     }else{
         return false;
